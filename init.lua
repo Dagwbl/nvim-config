ca63ebe6dcf86865c8f3c3e685ce0a12fc8f6251
Dagwbl <dagwbl@qq.com>
2026-01-18 12:57:49 -0700

    install lazynvim

---
+++ init.lua
@@ -0,0 +1,6 @@
+-- Set system locale to English UTF-8
+vim.env.LANG = "en_US.UTF-8"
+-- Set Neovim's language to English
+vim.cmd("language en_US")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
