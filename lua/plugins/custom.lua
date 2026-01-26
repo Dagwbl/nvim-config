6cbbf748615e106f5d3cc011b77438edc7c1ff9d
Dagwbl <dagwbl@qq.com>
2026-01-25 20:54:12 -0700

    remove notify in hardtime

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,32 +1,7 @@
 return {
   {
     "m4xshen/hardtime.nvim",
-    dependencies = { "rcarriga/nvim-notify" },
-    opts = {
-      notification = true,
-      callback = function(text)
-        require("notify")(text, "info", {
-          title = "Hardtime",
-          timeout = 2000,
-        })
-      end,
-    },
-  },
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      max_width = 80,
-      max_height = 10,
-      render = "default",
-      stages = "fade_in_slide_out",
-      top_down = true,
-    },
-  },
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
   },
 }
