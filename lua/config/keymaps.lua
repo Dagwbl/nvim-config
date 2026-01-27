0ec034be3f3cf8cb06c7d1a27daa18b0242e9d76
Dagwbl <dagwbl@qq.com>
2026-01-26 21:46:17 -0700

    diary: January 26, 2026 21:46

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-(1)
 vim.keymap.set("n", "<leader>db", function()
   Snacks.dashboard()
 end, { desc = "Open Dashboard" })
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Return Normal Mode" })
 vim.keymap.set("i", "jj", "<Esc>", { desc = "Return Normal Mode" })
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
