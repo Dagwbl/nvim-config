93ea2d0881e4a59cfadc1c60ace69cda624e6fff
Dagwbl <dagwbl@qq.com>
2026-01-29 22:27:17 -0700

    fix zotero integrity

--- lua/plugins/zotero.lua
+++ lua/plugins/zotero.lua
@@ -1,29 +1,25 @@
 return {
   {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      {
-        "jmbuhr/telescope-zotero.nvim",
-        dependencies = {
-          { "kkharji/sqlite.lua" },
-        },
-        opts = {
-          -- Use ~ for home directory - works on both Windows and Linux!
-          zotero_db_path = vim.fn.expand("~/Zotero/zotero.sqlite"),
-          better_bibtex_db_path = vim.fn.expand("~/Zotero/better-bibtex.sqlite"),
-          zotero_storage_path = vim.fn.expand("~/Zotero/storage"),
-        },
-      },
-    },
+    "kkharji/sqlite.lua",
+    lazy = false,
+    priority = 1000,
     config = function()
-      local telescope = require("telescope")
-      telescope.load_extension("zotero")
+      vim.g.sqlite_clib_path = vim.fn.stdpath("config") .. "/sqlite/sqlite3.dll"
     end,
   },
   {
-    "kkharji/sqlite.lua",
+    "jmbuhr/telescope-zotero.nvim",
+    dependencies = {
+      "kkharji/sqlite.lua",
+      "nvim-telescope/telescope.nvim",
+    },
+    event = "VeryLazy",
     config = function()
-      vim.g.sqlite_clib_path = vim.fn.stdpath("config") .. "/sqlite/sqlite3.dll"
+      require("zotero").setup({
+        zotero_db_path = vim.fn.expand("~/Zotero/zotero.sqlite"),
+        better_bibtex_db_path = vim.fn.expand("~/Zotero/better-bibtex.sqlite"),
+        zotero_storage_path = vim.fn.expand("~/Zotero/storage"),
+      })
     end,
   },
 }
