e42876362f82b34056c8e6cce6eeccf400535d0f
Dagwbl <dagwbl@qq.com>
2026-02-02 10:04:09 -0700

    set course path in neovide

--- init.lua
+++ init.lua
@@ -3,4 +3,7 @@
 -- Set Neovim's language to English
 -- vim.cmd("language en_US")
 -- bootstrap lazy.nvim, LazyVim and your plugins
+if vim.g.neovide then
+  vim.cmd("cd D:/jinpeng6/Documents/Course/Winter-2026/")
+end
 require("config.lazy")
