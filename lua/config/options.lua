2d201d156282a885ba9538b98f3e7f8cd6264a92
Dagwbl <dagwbl@qq.com>
2026-02-18 10:29:35 -0700

    change font to inter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,6 +6,10 @@
 vim.opt.wrap = true
 vim.opt.conceallevel = 0
 vim.opt.mouse = "a"
+vim.opt.linespace = 4
+vim.opt.spell = true
+vim.opt.spelllang = "en"
+vim.o.guifont = "Inter:h12"
 
 local hostname = vim.fn.hostname()
 if vim.g.neovide then
@@ -16,5 +20,5 @@ if vim.g.neovide then
     vim.cmd("cd D:/UA/Winter-2026/")
   end
 
-  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h12"
+  -- vim.o.guifont = "JetBrainsMono Nerd Font Mono:h12"
 end
