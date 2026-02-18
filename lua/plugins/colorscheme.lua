eae6ee5111686d0e940dcfbc02a5b8361f6567aa
Dagwbl <dagwbl@qq.com>
2026-02-17 21:26:11 -0700

    set catppuccin-latte as default theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-latte",
+    },
+  },
+
+  -- Gruvbox light/soft settings
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   config = function()
+  --     vim.o.background = "light"
+  --     vim.g.gruvbox_contrast_light = "soft"
+  --   end,
+  -- },
+}
