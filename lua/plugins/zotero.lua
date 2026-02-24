597ce2add6d982f7f1148b056c200d65bddc7569
Dagwbl <dagwbl@qq.com>
2026-02-23 19:33:58 -0700

    refactor: update Zotero plugin configuration and dependencies

--- lua/plugins/zotero.lua
+++ lua/plugins/zotero.lua
@@ -8,18 +8,10 @@ return {
     end,
   },
   {
-    "jmbuhr/telescope-zotero.nvim",
-    dependencies = {
-      "kkharji/sqlite.lua",
-      "nvim-telescope/telescope.nvim",
+    "jalvesaq/zotcite",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    opts = {
+      zotero_db_path = vim.g.my_paths.zotero_bib,
     },
-    event = "VeryLazy",
-    config = function()
-      require("zotero").setup({
-        zotero_db_path = vim.fn.expand("~/Zotero/zotero.sqlite"),
-        better_bibtex_db_path = vim.fn.expand("~/Zotero/better-bibtex.sqlite"),
-        zotero_storage_path = vim.fn.expand("~/Zotero/storage"),
-      })
-    end,
   },
 }
