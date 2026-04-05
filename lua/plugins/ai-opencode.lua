64bc06fcce59084dfa3d3a8153313c46fe8ed063
Dagwbl <dagwbl@qq.com>
2026-04-05 17:54:09 -0600

    change the shortcut for opencode and outline

--- lua/plugins/ai-opencode.lua
+++ lua/plugins/ai-opencode.lua
@@ -36,10 +36,10 @@ return {
       vim.o.autoread = true -- Required for `opts.events.reload`
 
       -- Recommended/example keymaps
-      vim.keymap.set({ "n", "x" }, "<C-a>", function()
+      vim.keymap.set({ "n", "x" }, "<leader>oa", function()
         require("opencode").ask("@this: ", { submit = true })
       end, { desc = "Ask opencode…" })
-      vim.keymap.set({ "n", "x" }, "<C-x>", function()
+      vim.keymap.set({ "n", "x" }, "<leader>ox", function()
         require("opencode").select()
       end, { desc = "Execute opencode action…" })
       vim.keymap.set({ "n", "t" }, "<C-.>", function()
@@ -61,8 +61,8 @@ return {
       end, { desc = "Scroll opencode down" })
 
       -- You may want these if you use the opinionated `<C-a>` and `<C-x>` keymaps above — otherwise consider `<leader>o…` (and remove terminal mode from the `toggle` keymap)
-      vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
-      vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
+      -- vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
+      -- vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
     end,
   },
 }
