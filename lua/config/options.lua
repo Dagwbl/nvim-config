d86ad0fa2bb13e98b6c76c6ea2a28d2227a0e0a8
Dagwbl <dagwbl@qq.com>
2026-06-07 23:32:13 -0600

    June 7

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Default options that are always set:
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
 
--- In lua/config/options.lua
 local hostname = vim.uv.os_gethostname()
 local my_paths = {}
 
@@ -19,24 +18,29 @@ elseif hostname == "WVLIU-GD15" then
   my_paths = {
     ollama_api = "http://localhost:11434",
     project_root = "~/personal/code",
-    term_path = "cd D:/jinpeng6/Documents/Course/Winter-2026/",
-    zotero_bib = "D:/jinpeng6/Documents/Course/Winter-2026/references.bib",
+    term_path = "cd D:/jinpeng6/Documents/UofA/",
+    zotero_bib = "D:/jinpeng6/Documents/UofA/data/References/references.bib",
     zotero_sqlite = "C:/Users/jinpeng6/Zotero/zotero.sqlite",
     zotero_storage = "C:/Users/jinpeng6/Zotero/storage/",
-    python = "C:/Users/jinpeng6/.conda/envs/main/python.exe",
+    python = "D:/jinpeng6/Documents/UofA/.venv/Scripts/python.exe",
     R_path = "C:/Users/jinpeng6/scoop/shims/R.exe",
   }
-else
 end
 
 vim.g.my_paths = my_paths
 vim.g.writing_ft = { markdown = true, quarto = true }
-vim.g.python3_host_prog = my_paths.python
+
+if my_paths.python then
+  vim.g.python3_host_prog = my_paths.python
+end
+
 if my_paths.R_path then
   vim.g.R_path = my_paths.R_path
 end
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
 vim.g.is_wezterm = os.getenv("WEZTERM_PANE") ~= nil or os.getenv("TERM_PROGRAM") == "WezTerm"
 
 vim.opt.mouse = "a"
@@ -44,15 +48,22 @@ vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.breakindent = true
 vim.opt.spelllang = { "en", "cjk" }
+
 vim.opt.guicursor:append("i-ci-ve:ver25-blinkwait500-blinkoff1000-blinkon1000")
 
 if vim.g.neovide then
-  vim.cmd(vim.g.my_paths.term_path)
+  if my_paths.term_path then
+    vim.cmd(my_paths.term_path)
+  end
+
   vim.opt.guifont = "Iosevka Nerd Font,IosevkaTermSlab Nerd Font Mono,JetBrainsMono Nerd Font Mono:h12"
-  vim.api.nvim_set_keymap("n", "<F11>", ":let g:neovide_fullscreen = !g:neovide_fullscreen<CR>", {})
+
+  vim.keymap.set("n", "<F11>", function()
+    vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
+  end, { desc = "Toggle Neovide fullscreen" })
+
   vim.g.neovide_cursor_animation_length = 0.1
   vim.g.neovide_cursor_smooth_blink = true
-  vim.g.neovide_opacity = 0.95
 else
   vim.opt.guifont =
     "Iosevka Nerd Font,IosevkaTermSlab Nerd Font Mono,Inter,JetBrainsMono Nerd Font Mono,Symbols Nerd Font Mono:h12"
